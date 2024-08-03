62b028cee835c4a3dc10fab71e46494068e4467c
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-08-03 16:21:53 +0200

    Create client.py
    creating the client class and adding class attributes

---
+++ Client/client.py
@@ -0,0 +1,9 @@
+class Client:
+    def __init__(self, nom, prenom, adresse, ville, code_postal, pays, comptes):
+        self.nom = nom
+        self.prenom = prenom
+        self.adresse = adresse
+        self.ville = ville
+        self.code_postal = code_postal
+        self.pays = pays
+        self.comptes = comptes
