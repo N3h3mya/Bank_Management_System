ad5a0e568db0f2cb5a2dd83295a2a879783b11b4
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-08-03 16:36:28 +0200

    Update client.py
    added display method for debugging

--- Client/client.py
+++ Client/client.py
@@ -1,9 +1,16 @@
 class Client:
-    def __init__(self, nom, prenom, adresse, ville, code_postal, pays, comptes):
+    def __init__(self, nom, prenom, adresse, ville, code_postal, pays, comptes=None):
+        
         self.nom = nom
         self.prenom = prenom
         self.adresse = adresse
         self.ville = ville
         self.code_postal = code_postal
         self.pays = pays
-        self.comptes = comptes
+        self.comptes = comptes if comptes is not None else []
+        
+    
+    #Méthode d'affichage pour le déboggage
+    def __repr__(self):
+        return f"{self.prenom} {self.nom} habite à {self.adresse}, {self.ville}, {self.code_postal}, {self.pays}"
+        
